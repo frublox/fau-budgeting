d4b2a52b1184aac5286b2f804a2067f058405b5e
frublox <lainislaw@gmail.com>
2017-11-15 16:56:58 -0500

    Start implementing template for admin page

---
+++ fau-budgeting/AdminModule.cs
@@ -0,0 +1,57 @@
+﻿using Nancy;
+using System.Collections.Generic;
+
+namespace fau_budgeting
+{
+    class BudgetRequest
+    {
+        public string Organization;
+        public string Date;
+        public string Status;
+    }
+    class BudgetRequestData
+    {
+        public List<BudgetRequest> Unreviewed;
+        public List<BudgetRequest> Reviewed;
+        public List<BudgetRequest> Accepted;
+    }
+
+    public class AdminModule : NancyModule
+    {
+        public AdminModule()
+        {
+            var data = new BudgetRequestData
+            {
+                Unreviewed = new List<BudgetRequest>
+                {
+                    new BudgetRequest()
+                    {
+                        Organization = "Blah",
+                        Date = "Today",
+                        Status = "New"
+                    }
+                },
+                Reviewed = new List<BudgetRequest>
+                {
+                    new BudgetRequest()
+                    {
+                        Organization = "Foo",
+                        Date = "Tomorrow",
+                        Status = "Waiting on Resubmission"
+                    }
+                },
+                Accepted = new List<BudgetRequest>
+                {
+                    new BudgetRequest()
+                    {
+                        Organization = "Bar",
+                        Date = "Yesterday",
+                        Status = "Accepted"
+                    }
+                }
+            };
+
+            Get["/admin"] = _ => View["admin", data];
+        }
+    }
+}
