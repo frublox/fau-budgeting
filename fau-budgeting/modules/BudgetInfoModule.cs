5524cd8ba05f92176c704dc89802cb4af398f99c
frublox <lainislaw@gmail.com>
2017-11-25 18:38:26 -0500

    Move rest of new modules

---
+++ fau-budgeting/modules/BudgetInfoModule.cs
@@ -0,0 +1,111 @@
+﻿using Nancy;
+using System.Collections.Generic;
+
+namespace fau_budgeting
+{
+    public class Xin
+    {
+        public int NumTagsIn;
+        public int[] TagsIn = { 750, 240, 100 };
+        public int TotalIn;
+        public int TagsInSum()
+        {
+            TotalIn = 0;
+            for (int i = 0; i < NumTagsIn; i++)
+            {
+                TotalIn = TotalIn + TagsIn[i];
+
+            }
+            return TotalIn;
+
+        }
+    }
+
+    public class Xout
+    {
+        public  int NumTagsOut;
+        public int[] TagsOut;
+        public int TotalOut;
+        public void TagsInSum()
+        {
+            TotalOut = 0;
+            for(int i=0; i<NumTagsOut; i++)
+            {
+                TotalOut = TotalOut + TagsOut[i];
+            }
+
+        }
+
+    }
+
+
+    public class BudgetInfo
+    {
+        public string ClubName;
+        public string SmartTag;
+        public string AccountName;
+        public string Fund;
+        public int Overhead;
+        public int Net;
+        
+
+    }
+
+    public class Expenses
+    {
+        public Expenses(int a, int b, int c, int d, int e, int f, int g)
+        {
+            Equipment = a;
+            Improvements = b;
+            Contingencies = c;
+            OfficeResource = d;
+            Program = e;
+            Services = f;
+            Travel = g;
+            TotalExpenses = a + b + c + d + e + f + g;
+        }
+        public int Equipment;
+        public int Improvements;
+        public int Contingencies;
+        public int OfficeResource;
+        public int Program;
+        public int Services;
+        public int Travel;
+        public int TotalExpenses;
+    }
+
+    
+
+
+
+    public class BudgetInfoModule : NancyModule
+    {
+        public BudgetInfoModule()
+        {
+            BudgetInfo info = new BudgetInfo
+            {
+                ClubName = "Robotics Club",
+                SmartTag = "Niner55312",
+                AccountName = "Robotics Club Council",
+                Fund = "Student Government Reserve",
+                Overhead = 2500,
+                Net = 80000,
+                  
+             
+
+            };
+
+            Xin info2 = new Xin();
+            info2.NumTagsIn = 3;
+            info2.TotalIn = info2.TagsInSum();
+            //.Console.WriteLine("Tags In Sum is " + info2.TotalIn);
+
+            Expenses exp = new Expenses(10, 20, 30, 40, 50, 60, 70);
+
+
+            //Get["/budget-request-info"] = _ => View["budgetinfo", exp];
+            Get["/budget-request-info"] = _ => View["budgetinfo", info2];
+            //Get["/budget-request-info"] = _ => View["budgetinfo", info];
+        }
+    }
+}
---
+++ fau-budgeting/modules/ReserveFund.cs
@@ -0,0 +1,56 @@
+﻿using Nancy;
+using System.Collections.Generic;
+
+namespace fau_budgeting
+{
+    public class ReserveFundSD
+    {
+        string SmartTag { get; set; }
+        string AcctName { get; set; }
+        struct XfersIn
+        {
+            string[] TagsIn { get; set; }
+            int[] TagsInAmount { get; set; }
+        }
+        int Equipment;
+        int Improvments;
+        int Contingencies;
+        struct XfersOut
+        {
+            string[] TagsOut { get; set; }
+            int[] TagsOutAmount { get; set; }
+        }
+
+    }
+
+    public class Supplemental_Form_Reserve
+    {
+        string SmartTag { get; set; }
+        string AcctName { get; set; }
+        public int ReqBud_Exp { get; set; }
+        public int AppBud_Exp { get; set; }
+        public int AppBud_X { get; set; }
+        public int ReqBud_X { get; set; }
+        public int ReqBud_OH { get; set; }
+        public int AppBud_OH { get; set; }
+        public int Xin_Amt { get; set; }
+        public int EquipmentIn_Amt { get; set; }
+        public int Improvements_Amt { get; set; }
+        public int Contingencies_Amt { get; set; }
+        public int Other_Amt { get; set; }
+        public int Xout_Amt { get; set; }
+
+
+    }
+
+
+    public class ReserveFund : NancyModule
+    {
+        public ReserveFund()
+        {
+
+
+            Get["/reserve-fund"] = _ => View["ReserveFund"];
+        }
+    }
+}
