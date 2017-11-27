fa260fcd0b6171d7b8e776e044b1a70ca6abc6fb
Daniel Escalante <descala1@fau.edu>
2017-11-27 14:43:11 -0500

    Reserve Fund template mostly completed, must interface w database to send json data.

--- fau-budgeting/ReserveFund.cs
+++ fau-budgeting/ReserveFund.cs
@@ -1,47 +1,42 @@
 ﻿using Nancy;
+using Nancy.ModelBinding;
 using System.Collections.Generic;
 using System.Web;
 using System.Web.Script.Serialization;
 using System.Linq;
+using System.IO;
+
+
 
 namespace fau_budgeting
 {
-    public class ReserveFundSD
+    public class Reserve_Request
     {
-        string SmartTag { get; set; }
-        string AcctName { get; set; }
-        struct XfersIn
-        {
-            string[] TagsIn { get; set; }
-            int[] TagsInAmount { get; set; }
-        }
-        int Equipment;
-        int Improvments;
-        int Contingencies;
-        struct XfersOut
-        {
-            string[] TagsOut { get; set; }
-            int[] TagsOutAmount { get; set; }
-        }
-
-    }
-
-    public class Supplemental_Form_Reserve
-    {
-        string SmartTag { get; set; }
-        string AcctName { get; set; }
-        public int ReqBud_Exp { get; set; }
-        public int AppBud_Exp { get; set; }
-        public int AppBud_X { get; set; }
-        public int ReqBud_X { get; set; }
-        public int ReqBud_OH { get; set; }
-        public int AppBud_OH { get; set; }
-        public int Xin_Amt { get; set; }
-        public int EquipmentIn_Amt { get; set; }
-        public int Improvements_Amt { get; set; }
-        public int Contingencies_Amt { get; set; }
-        public int Other_Amt { get; set; }
-        public int Xout_Amt { get; set; }
+        public string smarttag { get; set; }
+        public string accountname { get; set; }
+        public string transferin1 { get; set; }
+        public int transferin2 { get; set; }
+        public string transferin3 { get; set; }
+        public int transferin4 { get; set; }
+        public string transferin5 { get; set; }
+        public int transferin6 { get; set; }
+        public int equipment { get; set; }
+        public int improvements { get; set; }
+        public int contingencies1 { get; set; }
+        public string contingencies2 { get; set; }
+        public int contingencies3 { get; set; }
+        public string contingencies4 { get; set; }
+        public int contingencies5 { get; set; }
+        public string contingencies6 { get; set; }
+        public int contingencies7 { get; set; }
+        public string contingencies8 { get; set; }
+        public int contingencies9 { get; set; }
+        public string transferout1 { get; set; }
+        public int transferout2 { get; set; }
+        public string transferout3 { get; set; }
+        public int transferout4 { get; set; }
+        public string transferout5 { get; set; }
+        public int transferout6 { get; set; }
 
 
     }
@@ -53,24 +48,20 @@ namespace fau_budgeting
     {
         public ReserveFund()
         {
-            
 
+                     
 
-           Get["/reserve-fund"] = _ => View["ReserveFund"];
+            Get["/reserve-fund"] = _ => View["ReserveFund"];
 
-           Post["/reserve-fund/{smarttag}"] = parameters =>
-                {
-                    var dict = HttpUtility.ParseQueryString("");
-                    var json = new JavaScriptSerializer().Serialize(
-                            dict.Keys.Cast<string>()
-                                .ToDictionary(KeyNotFoundException => KeyNotFoundException, KeyNotFoundException => dict[KeyNotFoundException]));
-                    System.Console.WriteLine(json);
-                    System.Console.ReadLine();
-                    
-                   string smarttag = parameters.smarttag;
+            Post["/reserve-fund-submit"] = _ =>
+            {
+                Reserve_Request input = this.Bind<Reserve_Request>();
 
-                   return Negotiate;
-               };
+                var json = new JavaScriptSerializer().Serialize(input);
+                //var jsonStr = JSON.stringify(jsonVar);
+
+                return View["ReserveFund"];
+            };
 
             
 
