a01a44969430810e73d72e41e73917f7ee1112ec
frublox <lainislaw@gmail.com>
2017-12-03 16:54:44 -0500

    Implement login functionality + stuff

---
+++ fau-budgeting/modules/LoginModule.cs
@@ -0,0 +1,64 @@
+﻿using Nancy;
+using Nancy.Linker;
+using Nancy.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace fau_budgeting.modules
+{
+    class LoginFormData
+    {
+        public string Email;
+        public string Password;
+        public bool Remember;
+    }
+
+    class LoginResponseData
+    {
+        public string ErrorMessage;
+        public bool HasErrors;
+    }
+
+    public class LoginModule : NancyModule
+    {
+        public LoginModule(IResourceLinker linker)
+        {
+            Get["/"] = _ => View["login"];
+
+            Post["/login"] = _ =>
+            {
+                LoginFormData data = this.Bind<LoginFormData>();
+                User user = Database.GetUser(data.Email);
+
+                var responseData = new LoginResponseData
+                {
+                    HasErrors = true,
+                    ErrorMessage = "Invalid login credentials"
+                };
+
+                var invalidLoginView = View["login", responseData];
+
+                if (user == null)
+                {
+                    return invalidLoginView;
+                }
+
+                if (user.Type == "ASAB")
+                {
+                    return Response.AsRedirect("/admin");
+                }
+                else if (user.Type == "Organization")
+                {
+                    return Response.AsRedirect("/organization");
+                }
+                else
+                {
+                    return invalidLoginView;
+                }
+            };
+        }
+    }
+}
